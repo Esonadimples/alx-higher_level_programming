fbc8117b909c349e39496e4248e1ed823886a59a
Esonadimples <siyasandaa22@gmail.com>
2024-08-22 14:41:14 +0200

    0x03-python-data_structures

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# tuple from a sentence
+def multiple_returns(sentence):
+    """function returning a tuple with len of string and first char"""
+
+    kl_tuple = ()
+    if len(sentence) == 0:
+        kl_tuple = (0, None)
+    else:
+        kl_tuple = (len(sentence), sentence[0])
+    return kl_tuple
