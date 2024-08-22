6ef673f93196144aebc7cdf419a8a2d398488801
Esonadimples <siyasandaa22@gmail.com>
2024-08-22 03:02:46 +0200

    0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    Lastdigit = -(abs(number) % 10)
+else:
+    Lastdigit = (number % 10)
+if Lastdigit > 5:  # Assign message(Msg) in regard to last digit
+    Msg = "and is greater than 5"
+elif Lastdigit == 0:
+    Msg = "and is 0"
+else:
+    Msg = "and is less than 6 and not 0"
+print(F"Last digit of {number} is {Lastdigit} {Msg}")
---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+
+/**
+ *insert_node - Function that inserts a number into a sorted LL
+ *@head: a double pointer to the first node on the LL
+ *@number: the number to insert into a LL
+ *
+ *Return: Address of the newnode, NULL otherwise
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *temp, *prev, *newnode;
+
+	temp = (*head);
+	prev = NULL;
+
+	newnode = malloc(sizeof(newnode));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+
+	newnode->n = number;
+	newnode->next = NULL;
+
+	while (temp != NULL && temp->n < number)
+	{
+		prev = temp;
+		temp = temp->next;
+	}
+	/*insertion at the beginning*/
+	if (prev == NULL)
+	{
+		newnode->next = (*head);
+		(*head) = newnode;
+	}
+	/*insertion at the end */
+	else if (temp == NULL)
+	{
+		add_nodeint_end(head, number);
+	}
+	/*insertion in the middle*/
+	else
+	{
+		prev->next = newnode;
+		newnode->next = temp;
+	}
+
+	return (newnode);
+}
