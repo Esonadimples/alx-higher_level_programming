7d57c08bd9aff6c4c3635dd27d5d9c9937551423
Esonadimples <siyasandaa22@gmail.com>
2024-08-23 16:48:01 +0200

    python classes

---
+++ 0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""creating an empty class with just 3 lines of code """
+
+# No module is to be imported
+
+
+class Square:
+    """This is an empty class that defines a square
+    a class is simply created by using the keyword class then
+    followed by a class name"""
+    pass
