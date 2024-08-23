a51b7c9a622abac138f23c26b8bcd656cb538a62
Esonadimples <siyasandaa22@gmail.com>
2024-08-23 12:05:28 +0200

    python exceptions

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+    """ bytcode function doing magic calculation
+    it return a / b ** i"""
+
+    resul = 0
+
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                resul += (a ** b) / i
+
+        except Exception:
+            resul = a + b
+            break
+
+    return (resul)
